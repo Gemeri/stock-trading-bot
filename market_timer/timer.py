59e1672bf1871481d12f3329b67eca7bdc2e58ab
Gemeri <jeremy.fratini@gmail.com>
2026-01-17 12:55:06 +1100

    timer

--- market_timer/timer.py
+++ market_timer/timer.py
@@ -1,6 +1,7 @@
+import pandas as pd
 
-def get_execution_decision(df, ticker, label, model):
+def get_execution_decision(df: pd.DataFrame, ticker: str, label: int, model: str, direction: int):
     print("TODO")
 
-def execution_backtest(df, ticker, label, model):
+def execution_backtest(df: pd.DataFrame, ticker: str, label: int, model: str, direction: int):
     print("TODO")
